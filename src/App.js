5011942a7bedbdfa12a4b3440840386f4beb4149
Jason P'ng <jsjpng@gmail.com>
2017-05-27 01:18:08 -0400

    Added basic forms to front page

--- src/App.js
+++ src/App.js
@@ -8,11 +8,23 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to WatisClass</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form>
+          Class Name:<br />
+          <input type="text" /> <br />
+          # of pages:<br />
+          <input type="text" />
+          <input type="submit" value="Make Class" />
+        </form>
+        <form id="form">
+          Class Code:<br />
+          <input type="text" />
+          <input type="submit" value="Join Class" />
+        </form>
       </div>
     );
   }
---
+++ src/ClassPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ClassPage extends Component {
+	render() {
+		return (
+			<p>
+				GOT A Class
+			</p>
+		)
+	}
+
+}
