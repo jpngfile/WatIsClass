86b9e5c44312788cb131047f7dca0ba61675336a
Jason P'ng <jsjpng@gmail.com>
2017-05-27 03:53:10 -0400

    Removed unnecessary files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import ClassPage from './ClassPage.js';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-ReactDOM.render(<ClassPage />, document.getElementById('root'));
-registerServiceWorker();
