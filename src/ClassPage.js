117a92950d0400cde7cfcd591ef320a185263a21
Jason P'ng <jsjpng@gmail.com>
2017-05-27 01:52:07 -0400

    Added new file to top

--- src/ClassPage.js
+++ src/ClassPage.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
 
-class ClassPage extends Component {
+import React, { Component } from 'react';
+import './ClassPage.css';
+
+export default class ClassPage extends Component {
 	render() {
 		return (
 			<p>
 				GOT A Class
 			</p>
-		)
+		);
 	}
-
 }
