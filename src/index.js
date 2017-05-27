496880dafaca30cf5bd5286029f204ddb31c7a23
[Felices] <felixcheng232@gmail.com>
2017-05-27 15:50:08 -0400

    The Class Pages and stuff

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ClassPage from './ClassPage.js';
+import LecturePage from './LecturePage.js';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<ClassPage />, document.getElementById('root'));
+ReactDOM.render(<LecturePage />, document.getElementById('root'));
 registerServiceWorker();
